bb90ffb4863d4caa84dfb26ed6006d0868f8fbf9
김현욱 <company@wiselight.kr>
2023-05-05 20:44:13 +0900

    chore: WebSocket 추가

--- constants/wiselightNodesRpcs.js
+++ constants/wiselightNodesRpcs.js
@@ -8,12 +8,22 @@ export const wiselightNodesRpcs = {
         tracking: 'none',
         trackingDetails: privacyStatement,
       },
+      {
+        url: "wss://node.wiselight.kr/1/ws",
+        tracking: 'none',
+        trackingDetails: privacyStatement,
+      },
     ],
   },
   10: {
     rpcs: [
       {
         url: "https://node.wiselight.kr/10/rpc",
+        tracking: 'none',
+        trackingDetails: privacyStatement,
+      },
+      {
+        url: "wss://node.wiselight.kr/10/ws",
         tracking: 'none',
         trackingDetails: privacyStatement,
       },
@@ -26,6 +36,11 @@ export const wiselightNodesRpcs = {
         tracking: 'none',
         trackingDetails: privacyStatement,
       },
+      {
+        url: "wss://node.wiselight.kr/56/ws",
+        tracking: 'none',
+        trackingDetails: privacyStatement,
+      },
     ],
   },
   137: {
@@ -35,12 +50,22 @@ export const wiselightNodesRpcs = {
         tracking: 'none',
         trackingDetails: privacyStatement,
       },
+      {
+        url: "wss://node.wiselight.kr/137/ws",
+        tracking: 'none',
+        trackingDetails: privacyStatement,
+      },
     ],
   },
   42161: {
     rpcs: [
       {
         url: "https://node.wiselight.kr/42161/rpc",
+        tracking: 'none',
+        trackingDetails: privacyStatement,
+      },
+      {
+        url: "wss://node.wiselight.kr/42161/ws",
         tracking: 'none',
         trackingDetails: privacyStatement,
       },
