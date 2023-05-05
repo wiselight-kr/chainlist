5664a8767ecd8ff241e810fef7caddf7d00149ac
김현욱 <company@wiselight.kr>
2023-05-05 23:44:25 +0900

    chore: Node 주소에 키 넣기

--- constants/wiselightNodesRpcs.js
+++ constants/wiselightNodesRpcs.js
@@ -4,12 +4,12 @@ export const wiselightNodesRpcs = {
   1: {
     rpcs: [
       {
-        url: "https://node.wiselight.kr/1/rpc",
+        url: "https://node.wiselight.kr/b49dc7f6a3f2bdf2de6f1b8bee87e1c60db7dd0c/1/rpc",
         tracking: 'none',
         trackingDetails: privacyStatement,
       },
       {
-        url: "wss://node.wiselight.kr/1/ws",
+        url: "wss://node.wiselight.kr/b49dc7f6a3f2bdf2de6f1b8bee87e1c60db7dd0c/1/ws",
         tracking: 'none',
         trackingDetails: privacyStatement,
       },
@@ -18,12 +18,12 @@ export const wiselightNodesRpcs = {
   10: {
     rpcs: [
       {
-        url: "https://node.wiselight.kr/10/rpc",
+        url: "https://node.wiselight.kr/b49dc7f6a3f2bdf2de6f1b8bee87e1c60db7dd0c/10/rpc",
         tracking: 'none',
         trackingDetails: privacyStatement,
       },
       {
-        url: "wss://node.wiselight.kr/10/ws",
+        url: "wss://node.wiselight.kr/b49dc7f6a3f2bdf2de6f1b8bee87e1c60db7dd0c/10/ws",
         tracking: 'none',
         trackingDetails: privacyStatement,
       },
@@ -32,12 +32,12 @@ export const wiselightNodesRpcs = {
   56: {
     rpcs: [
       {
-        url: "https://node.wiselight.kr/56/rpc",
+        url: "https://node.wiselight.kr/b49dc7f6a3f2bdf2de6f1b8bee87e1c60db7dd0c/56/rpc",
         tracking: 'none',
         trackingDetails: privacyStatement,
       },
       {
-        url: "wss://node.wiselight.kr/56/ws",
+        url: "wss://node.wiselight.kr/b49dc7f6a3f2bdf2de6f1b8bee87e1c60db7dd0c/56/ws",
         tracking: 'none',
         trackingDetails: privacyStatement,
       },
@@ -46,12 +46,12 @@ export const wiselightNodesRpcs = {
   137: {
     rpcs: [
       {
-        url: "https://node.wiselight.kr/137/rpc",
+        url: "https://node.wiselight.kr/b49dc7f6a3f2bdf2de6f1b8bee87e1c60db7dd0c/137/rpc",
         tracking: 'none',
         trackingDetails: privacyStatement,
       },
       {
-        url: "wss://node.wiselight.kr/137/ws",
+        url: "wss://node.wiselight.kr/b49dc7f6a3f2bdf2de6f1b8bee87e1c60db7dd0c/137/ws",
         tracking: 'none',
         trackingDetails: privacyStatement,
       },
@@ -60,12 +60,12 @@ export const wiselightNodesRpcs = {
   42161: {
     rpcs: [
       {
-        url: "https://node.wiselight.kr/42161/rpc",
+        url: "https://node.wiselight.kr/b49dc7f6a3f2bdf2de6f1b8bee87e1c60db7dd0c/42161/rpc",
         tracking: 'none',
         trackingDetails: privacyStatement,
       },
       {
-        url: "wss://node.wiselight.kr/42161/ws",
+        url: "wss://node.wiselight.kr/b49dc7f6a3f2bdf2de6f1b8bee87e1c60db7dd0c/42161/ws",
         tracking: 'none',
         trackingDetails: privacyStatement,
       },
@@ -74,7 +74,7 @@ export const wiselightNodesRpcs = {
   43114: {
     rpcs: [
       {
-        url: "https://node.wiselight.kr/43114/rpc",
+        url: "https://node.wiselight.kr/b49dc7f6a3f2bdf2de6f1b8bee87e1c60db7dd0c/43114/rpc",
         tracking: 'none',
         trackingDetails: privacyStatement,
       },
