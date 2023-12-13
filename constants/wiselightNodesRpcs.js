f2d5adbe9cb0bde0eb2c12e81abd7fc09534abde
김현욱 <company@wiselight.kr>
2023-12-13 14:21:04 +0900

    chore: demo 키 적용 및 ws 제거

--- constants/wiselightNodesRpcs.js
+++ constants/wiselightNodesRpcs.js
@@ -4,12 +4,7 @@ export const wiselightNodesRpcs = {
   1: {
     rpcs: [
       {
-        url: "https://node.wiselight.kr/b49dc7f6a3f2bdf2de6f1b8bee87e1c60db7dd0c/1/rpc",
-        tracking: 'none',
-        trackingDetails: privacyStatement,
-      },
-      {
-        url: "wss://node.wiselight.kr/b49dc7f6a3f2bdf2de6f1b8bee87e1c60db7dd0c/1/ws",
+        url: "https://node.wiselight.kr/demo/1/rpc",
         tracking: 'none',
         trackingDetails: privacyStatement,
       },
@@ -18,12 +13,7 @@ export const wiselightNodesRpcs = {
   10: {
     rpcs: [
       {
-        url: "https://node.wiselight.kr/b49dc7f6a3f2bdf2de6f1b8bee87e1c60db7dd0c/10/rpc",
-        tracking: 'none',
-        trackingDetails: privacyStatement,
-      },
-      {
-        url: "wss://node.wiselight.kr/b49dc7f6a3f2bdf2de6f1b8bee87e1c60db7dd0c/10/ws",
+        url: "https://node.wiselight.kr/demo/10/rpc",
         tracking: 'none',
         trackingDetails: privacyStatement,
       },
@@ -32,12 +22,7 @@ export const wiselightNodesRpcs = {
   56: {
     rpcs: [
       {
-        url: "https://node.wiselight.kr/b49dc7f6a3f2bdf2de6f1b8bee87e1c60db7dd0c/56/rpc",
-        tracking: 'none',
-        trackingDetails: privacyStatement,
-      },
-      {
-        url: "wss://node.wiselight.kr/b49dc7f6a3f2bdf2de6f1b8bee87e1c60db7dd0c/56/ws",
+        url: "https://node.wiselight.kr/demo/56/rpc",
         tracking: 'none',
         trackingDetails: privacyStatement,
       },
@@ -46,12 +31,7 @@ export const wiselightNodesRpcs = {
   137: {
     rpcs: [
       {
-        url: "https://node.wiselight.kr/b49dc7f6a3f2bdf2de6f1b8bee87e1c60db7dd0c/137/rpc",
-        tracking: 'none',
-        trackingDetails: privacyStatement,
-      },
-      {
-        url: "wss://node.wiselight.kr/b49dc7f6a3f2bdf2de6f1b8bee87e1c60db7dd0c/137/ws",
+        url: "https://node.wiselight.kr/demo/137/rpc",
         tracking: 'none',
         trackingDetails: privacyStatement,
       },
@@ -60,12 +40,7 @@ export const wiselightNodesRpcs = {
   42161: {
     rpcs: [
       {
-        url: "https://node.wiselight.kr/b49dc7f6a3f2bdf2de6f1b8bee87e1c60db7dd0c/42161/rpc",
-        tracking: 'none',
-        trackingDetails: privacyStatement,
-      },
-      {
-        url: "wss://node.wiselight.kr/b49dc7f6a3f2bdf2de6f1b8bee87e1c60db7dd0c/42161/ws",
+        url: "https://node.wiselight.kr/demo/42161/rpc",
         tracking: 'none',
         trackingDetails: privacyStatement,
       },
@@ -74,7 +49,7 @@ export const wiselightNodesRpcs = {
   43114: {
     rpcs: [
       {
-        url: "https://node.wiselight.kr/b49dc7f6a3f2bdf2de6f1b8bee87e1c60db7dd0c/43114/rpc",
+        url: "https://node.wiselight.kr/demo/43114/rpc",
         tracking: 'none',
         trackingDetails: privacyStatement,
       },
